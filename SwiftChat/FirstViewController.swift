090b3eb33d11f9a37b6ae6112149c50593617ee6
Yosei Ito <yousei.itou@gmail.com>
2015-04-14 09:52:23 +0900

    読み込み機能を実装

--- SwiftChat/FirstViewController.swift
+++ SwiftChat/FirstViewController.swift
@@ -14,6 +14,9 @@ class FirstViewController: UIViewController,UITableViewDataSource {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sendButton: UIButton!
     @IBOutlet weak var table: UITableView!
+    
+    var data: NSMutableArray = NSMutableArray()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,6 +31,7 @@ class FirstViewController: UIViewController,UITableViewDataSource {
         }else{
             nameLabel.text = name
         }
+        reloadChats()
     }
     
     override func didReceiveMemoryWarning() {
@@ -37,16 +41,72 @@ class FirstViewController: UIViewController,UITableViewDataSource {
 
     func tableView(tableView: UITableView,
         cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-            let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
-            
-            // Configure the cell...
-            
+            let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+            if let row = data.objectAtIndex(indexPath.row) as? Dictionary<String,String> {
+                cell.textLabel!.text = row["message"]
+                cell.detailTextLabel!.text = row["user"]! + " " + row["created_at"]!
+            }
             return cell
     }
 
     func tableView(tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int{
-            return 0;
+            return data.count;
+    }
+    
+    func reloadChats(){
+        self.data = NSMutableArray()
+        
+        let URL = "http://chat.lmlab.net/chats.json"
+        let url: NSURL? = NSURL(string: URL)
+        if url == nil {
+            NSLog("Invalid constant. %@", URL)
+            return;
+        }
+        let request: NSURLRequest = NSURLRequest(URL: url!)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler: {
+            (resp:NSURLResponse?, data:NSData?, e1:NSError?) -> Void in
+            if e1 != nil {
+                NSLog("%@", e1!.description)
+                return
+            }
+            if data == nil {
+                NSLog("data is null")
+                return
+            }
+            var e2: NSError?
+            if let chats = NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: &e2) as? NSArray {
+                for c in chats {
+                    var row = ["user":"","message":"","created_at":""]
+                    if let user = c["user"] as? String {
+                        row["user"] = user
+                        println(user)
+                    }
+                    if let message = c["message"] as? String {
+                        row["message"] = message
+                        println(message)
+                    }
+                    if let created_at = c["created_at"] as? String {
+                        row["created_at"] = created_at
+                    }
+                    self.data.addObject(row)
+                }
+            }
+            NSLog("%d rows loaded.", self.data.count)
+            dispatch_async(dispatch_get_main_queue(), {
+                self.table.reloadData()
+            })
+        });
+    }
+    
+    @IBAction func sendPushed(sender: AnyObject) {
+        textField.resignFirstResponder()
+        let name = nameLabel.text
+        let text = textField.text
+        if !text.isEmpty {
+            // TODO: Send text
+        }
+        reloadChats()
     }
 }
 
